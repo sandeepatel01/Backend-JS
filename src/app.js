721137db64c257c951870e540b3df4e2faafa164
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-02-12 19:30:00 +0530

    add bug fixes

--- src/app.js
+++ src/app.js
@@ -11,16 +11,17 @@ app.use(cors({
 }));
 
 
-// routes import 
-import userRouter from "./routes/user.route.js";
-
-// routes declaration 
-app.use("/user/api/v1/users", userRouter);
-
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+
+// routes import 
+import userRouter from "./routes/user.route.js";
+
+// routes declaration 
+app.use("/api/v1/users", userRouter);
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // require('dotenv').config({ path: './env' });  // code readiability not good
 
 import dotenv from "dotenv";
-dotenv.config({
-    path: './env'
-});
 import connectDB from "./db/db.js";
-
+import { app } from "./app.js";
+dotenv.config({
+    path: './.env'
+});
 
 
 connectDB()
     .then(() => {
-        app.listrn(process.env.PORT || 3000, () => {
-            console.log(`Server is running at Port: ${process.env.PORT}`);
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`⚙️  Server is running at Port: ${process.env.PORT}`);
         })
     })
     .catch((error) => {

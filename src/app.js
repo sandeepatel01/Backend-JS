7f0d86e937e6d119fd8f46462577379cf36fbf80
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-02-12 18:26:30 +0530

    add users registation controller

--- src/app.js
+++ src/app.js
@@ -8,7 +8,14 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}));
+
+
+// routes import 
+import userRouter from "./routes/user.route.js";
+
+// routes declaration 
+app.use("/user/api/v1/users", userRouter);
 
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));

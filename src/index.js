7e1c258ad87e7f2711d97f654a0f76a70b6b670e
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-06-17 19:28:01 +0530

    Add DB connection & Express server file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import { DB_NAME } from "./constants.js";
 import dotenv from "dotenv";
-import connectDB from "./db/db";
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
      path: './env'
 });
 
 connectDB()
+     .then(() => {
+          app.on("error", (error) => {
+               console.log("Error: ", error);
+               throw error
+          });
+
+          app.listen(process.env.PORT || 3000, () => {
+               console.log(` ⚙️  Server is Running at port: ${process.env.PORT}`);
+          });
+
+     }).catch((error) => {
+          console.log("MongoDB Connection FAILED!", error);
+     })

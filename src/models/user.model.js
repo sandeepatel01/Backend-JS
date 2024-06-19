d66fb5b9249f38f3490c9a090815ad57813a933b
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-06-19 22:24:37 +0530

    Add user model file

---
+++ src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+     {
+          username: {
+               type: String,
+               required: true
+          },
+          email: {
+               type: String,
+               required: true,
+               unique: true
+          },
+          fullname: {
+               type: String,
+               required: true
+          },
+          username: {
+               type: String,
+               required: true,
+               unique: true
+          }
+     }
+)
+
+export const User = mongoose.model("User", userSchema);

d68ced81e5f6638c978a228a29158debfaf0dcb3
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-06-20 18:20:17 +0530

    Add mongoose plugin

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema, Types } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const aggregatePaginate = require('mongoose-aggregate-paginate-v2');
 
 const videoSchema = new mongoose.Schema(
      {
@@ -33,5 +35,7 @@ const videoSchema = new mongoose.Schema(
      },
      { timestamps: true }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
